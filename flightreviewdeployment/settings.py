6ef596ee205253d29daa368da6bd4b025789f535
akshayerk12 <akshayerk12@gmail.com>
2023-11-10 12:22:59 +0530

    commit after updating database to psql

--- flightreviewdeployment/settings.py
+++ flightreviewdeployment/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'flightreviewdeployment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Airlines',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 
